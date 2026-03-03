d2685584405c6c655c3d6e129739d59737c4437a
DAVI CHICARELLI PEREIRA <davi.cpereira4@senacsp.edu.br>
2026-03-03 14:27:30 -0300

    commit inicial. Atividade String. Feita por Davi e Juan.

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Formulário - Manipulação de Strings</title>
+</head>
+<body>
+
+<h2>Formulário de Cadastro</h2>
+
+<form action="processa.php" method="POST">
+
+    Nome completo:<br>
+    <input type="text" name="nome" required>
+    <br><br>
+
+    Email:<br>
+    <input type="text" name="email" required>
+    <br><br>
+
+    Frase de apresentação:<br>
+    <input type="text" name="frase" required>
+    <br><br>
+
+    <button type="submit">Enviar</button>
+
+</form>
+
+</body>
+</html>
